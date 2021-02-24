412bffba170396f1aa71ce4d74a02a783008f8be
vini-miranda <v-alves-miranda@live.com>
2021-02-24 11:09:07 -0300

    finalizando páginas estáticas

--- frontAda/src/app/app-routing.module.ts
+++ frontAda/src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { PrincipalComponent } from './principal/principal.component';
+
 
 const routes: Routes = [
   {path:'',redirectTo:'entrar', pathMatch:'full'},
  
   {path:'entrar',component:EntrarComponent},
   {path:'cadastrar', component:CadastrarComponent},
-
-  /*{path: 'main', component: MainComponent},*/
-  /*{path: 'principal', component: PrincipalComponent},*/
+  {path:'principal', component: PrincipalComponent}
 ];
 
 @NgModule({
--- frontAda/src/app/app.module.ts
+++ frontAda/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { PrincipalComponent } from './principal/principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EntrarComponent,
-    CadastrarComponent
+    CadastrarComponent,
+    InicioComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
