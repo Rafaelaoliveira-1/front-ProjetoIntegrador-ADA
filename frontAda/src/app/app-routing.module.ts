52f20e86474d0abceaa042e6b8191c0bf821c270
DaniloMendesF <danilmendes@gmail.com>
2021-02-26 17:42:43 -0300

    Criação da página home

--- frontAda/src/app/app-routing.module.ts
+++ frontAda/src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { HomeComponent } from './home/home.component';
 import { PrincipalComponent } from './principal/principal.component';
 
 
 const routes: Routes = [
-  {path:'',redirectTo:'entrar', pathMatch:'full'},
+  {path:'',redirectTo:'home', pathMatch:'full'},
  
   {path:'entrar',component:EntrarComponent},
   {path:'cadastrar', component:CadastrarComponent},
-  {path:'principal', component: PrincipalComponent}
+  {path:'principal', component: PrincipalComponent},
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
