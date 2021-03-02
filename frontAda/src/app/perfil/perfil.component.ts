0f296feb0324cc0b8af333a18900697cee73ad5a
beymarjhoel <beymarjhlima@gmail.com>
2021-03-02 20:06:28 -0300

    Iniciando perfil

--- frontAda/src/app/perfil/perfil.component.ts
+++ frontAda/src/app/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  nomeCompleto = environment.nomeCompleto
+  foto = environment.foto
+  cargo = environment.cargo
+  link = environment.link
 
-  ngOnInit(): void {
+  constructor (
+
+    private router: Router, 
+
+  ) { }
+
+  ngOnInit() {
+
+    if (environment.token == '') {
+      alert('Sua sessão expirou!')
+      this.router.navigate(['/entrar'])
+    } 
+
   }
 
 }
