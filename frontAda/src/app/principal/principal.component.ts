db029291b1f58ad411fab75fd3bba37e229733bb
DaniloMendesF <danilmendes@gmail.com>
2021-03-03 13:12:59 -0300

    Consumo das postagens e alterações de layout

--- frontAda/src/app/principal/principal.component.ts
+++ frontAda/src/app/principal/principal.component.ts
@@ -4,7 +4,6 @@ import { environment } from 'src/environments/environment.prod';
 import { Postagem } from '../model/Postagem';
 import { Tema } from '../model/Tema';
 import { Usuario } from '../model/Usuario';
-import { AuthService } from '../service/auth.service';
 import { PostagemService } from '../service/postagem.service';
 import { TemaService } from '../service/tema.service';
 
@@ -15,28 +14,28 @@ import { TemaService } from '../service/tema.service';
 })
 export class PrincipalComponent implements OnInit {
 
+  user:Usuario = new Usuario()
   postagem:Postagem = new Postagem()
-  listaPostagem: Postagem[]
-  
   tema:Tema = new Tema()
-  listaTema:Tema[]
-  tipoTema: string
 
-  user:Usuario= new Usuario()
+  listaPostagem:Postagem[]
+  listaTema:Tema[]
+
+  tipoTema: string
+  tipoPostagem: string
 
   idTema:number
   idUser = environment.id
+  idcheck:number
 
   nomeCompleto = environment.nomeCompleto
   foto = environment.foto
   cargo = environment.cargo
 
-
   constructor(
     private router: Router,
     private TemaService: TemaService,
-    private postagemService: PostagemService,
-    private authService: AuthService
+    private PostagemService: PostagemService
   ) {}
 
   ngOnInit() {
@@ -47,7 +46,12 @@ export class PrincipalComponent implements OnInit {
     }
     this.findAllTema()
     this.findAllPostagem()
+  }
 
+  findAllPostagem(){
+    this.PostagemService.getAllPostagem().subscribe((resp: Postagem[])=>{
+      this.listaPostagem = resp.reverse()
+    })
   }
 
   findAllTema(){
@@ -60,15 +64,16 @@ export class PrincipalComponent implements OnInit {
     this.tipoTema = event.target.value
   }
 
-  findAllPostagem(){
-    this.postagemService.getAllPostagem().subscribe((resp:Postagem[]) =>{
-      this.listaPostagem = resp
+  selecionar(event:any){
+    this.idTema = event.target.id
+    this.TemaService.getByIdTema(this.idTema).subscribe((resp:Tema) =>{
+    this.tema = resp
     })
   }
 
   cadastrar(){
     this.tema.tipoTema = this.tipoTema
-
+  
     this.TemaService.postTema(this.tema).subscribe((resp:Tema) =>{
       this.tema = resp
       alert('Cadastrado com sucesso!')
@@ -77,30 +82,17 @@ export class PrincipalComponent implements OnInit {
     })
   }
 
-  selecionar(){
-    this.TemaService.getByIdTema(this.idTema).subscribe((resp: Tema) =>{
-    this.tema = resp
-    })
-  }
-
-  findByIdUser(){
-    this.authService.getByIdUser(this.idUser).subscribe((resp: Usuario) =>{
-      this.user = resp
-    })
-  }
-  
   postar(){
     this.tema.id = this.idTema
     this.postagem.tema = this.tema
     this.user.id = this.idUser
     this.postagem.usuario = this.user
 
-    this.postagemService.postPostagem(this.postagem).subscribe((resp:Postagem) =>{
+    this.PostagemService.postPostagem(this.postagem).subscribe((resp:Postagem) =>{
       this.postagem = resp
-      alert('Publicação realizada com sucesso!')
-      this.postagem = new Postagem() //Fazer teste depois
+      alert('Postagem realizada com sucesso!')
+      this.postagem = new Postagem()
       this.findAllPostagem()
     })
   }
-
 }
--- frontAda/src/app/service/postagem.service.ts
+++ frontAda/src/app/service/postagem.service.ts
@@ -10,25 +10,26 @@ import { Postagem } from '../model/Postagem';
 export class PostagemService {
 
   constructor(
-    private http: HttpClient
+    private http:HttpClient
   ) { }
 
   token = {
-    headers: new HttpHeaders().set('Authorization', environment.token)
-  }
+    headers:new HttpHeaders().set('Authorization',environment.token)
+    }
+  
   getAllPostagem(): Observable<Postagem[]>{
     return this.http.get<Postagem[]>('http://localhost:8080/postagem',this.token)
   }
   getByIdPostagem(id:number): Observable<Postagem>{
-    return this.http.get<Postagem>(`http://localhost:8080/postagem/${id}`,this.token)  
+    return this.http.get<Postagem>(`http://localhost:8080/postagem/${id}`,this.token)
   }
   postPostagem(postagem:Postagem): Observable<Postagem>{
     return this.http.post<Postagem>('http://localhost:8080/postagem',postagem,this.token)
   }
-  putPostagem(postagem:Postagem): Observable<Postagem>{
-    return this.http.put<Postagem>('http://localhost:8080/postagem',postagem,this.token)
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>('http://localhost:8080/postagem',postagem, this.token)
   }
   deletePostagem(id:number){
     return this.http.delete(`http://localhost:8080/postagem/${id}`,this.token)
-  }
+  }   
 }
