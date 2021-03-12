b3c40f34c23fbc7701bd5dc465a180b9a7a8674a
DaniloMendesF <danilmendes@gmail.com>
2021-03-11 22:12:52 -0300

    responsividade

--- frontAda/src/app/principal/principal.component.ts
+++ frontAda/src/app/principal/principal.component.ts
@@ -56,10 +56,6 @@ export class PrincipalComponent implements OnInit {
   ngOnInit() {
 
     
-    if (environment.token == '') {
-      this.alertas.showAlertInfo('Sua sessão expirou!')
-      this.router.navigate(['/entrar'])
-    }
     this.getNews()
 
     this.findAllPostagem()
